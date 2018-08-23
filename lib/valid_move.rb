f318489758cf1c5379f9402f881ef0798cad8dbd
Kristen Robertson <kristen77@gmail.com>
2018-08-23 03:05:31 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,6 +2,8 @@
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
 def valid_move?(board, index)
+  positon on board is empty, box 1-8 
+  
   if board[index].between(1,8) == " " && !position_taken(board, index)  
      return true
   
