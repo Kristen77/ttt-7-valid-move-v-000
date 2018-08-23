1d5a6064b2b90e4cc03359c0bc09375536103adc
Kristen Robertson <kristen77@gmail.com>
2018-08-23 02:28:20 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -3,11 +3,11 @@ board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
 def valid_move?(board, index)
 def position_taken?(board, index) 
-  if 'board[index]'.between(1,8) == " " && !position_taken(board, index)  
+  if board[index].between(1,8) == " " && !position_taken(board, index)  
      return true
   
 elsif
-  'board[index]'.between(1,8) == "X"
+  board[index].between(1,8) == "X"
    return false
     
 elseif
