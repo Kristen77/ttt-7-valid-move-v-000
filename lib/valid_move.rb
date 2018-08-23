f37d123420f0c0641d9cdb04d45658c266e87592
Kristen Robertson <kristen77@gmail.com>
2018-08-23 03:12:29 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,7 +2,7 @@
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
 def valid_move?(board, index)
-  !position_taken?(board, index) && index.between(1,8)  
+  !position_taken?(board, index) && index.between?(1,8)  
 end  
 
 def position_taken?(board, index)
